52297bfab7ff00101ce9531c226256554fe385bc
barasher <arnaud.barasher@gmail.com>
2021-11-21 23:27:58 +0100

    Fix bugs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
 import json
+import csv
+import calendar
 
 
 class Project:
@@ -17,24 +19,64 @@ class Assignment:
         self.end = end
 
 
-class Manager:
+class TeamProjPercent:
 
-    def __init__(self, start: datetime.date, end: datetime.date, team_proj_percent: str):
-        self.start = start
-        self.end = end
-        self.projects = dict()
-        self.agents_by_project_and_date = dict()  # projId -> day -> list<agents>
-        self.assignment_count_by_day_and_agent = dict()  # projId_YYYYMMDD -> count
-
-        with open(team_proj_percent) as j:
-            data = json.load(j)
-        self.proj_percentage_by_agent = dict(data)
+    def __init__(self, agent_to_percent: dict):
+        self.proj_percentage_by_agent = agent_to_percent
 
     def get_agent_project_percentage(self, agent: str):
         try:
             return self.proj_percentage_by_agent[agent]
         except KeyError:
             raise Exception("Can't find project percentage for agent {}".format(agent))
+
+    @staticmethod
+    def load(f: str):
+        with open(f) as j:
+            data = json.load(j)
+        return TeamProjPercent(data)
+
+    @staticmethod
+    def empty():
+        return TeamProjPercent(dict())
+
+
+class Holidays:
+    def __init__(self, d: dict):
+        self.data = d
+
+    @staticmethod
+    def load(f: str):
+        d = dict()
+        with open(f) as j:
+            r = csv.DictReader(j, delimiter=";")
+            for line in r:
+                for k, v in line.items():
+                    if k != "Agent":
+                        d["{}_{}".format(line["Agent"], k)] = int(v)
+        return Holidays(d)
+
+    @staticmethod
+    def empty():
+        return Holidays(dict())
+
+    def get(self, who: str, month: str):
+        try:
+            return self.data["{}_{}".format(who, month)]
+        except KeyError:
+            return 0
+
+
+class Manager:
+
+    def __init__(self, start: datetime.date, end: datetime.date, tpp: TeamProjPercent, h: Holidays):
+        self.start = start
+        self.end = end
+        self.projects = dict()
+        self.agents_by_project_and_date = dict()  # projId -> day -> list<agents>
+        self.assignment_count_by_day_and_agent = dict()  # projId_YYYYMMDD -> count
+        self.team_proj_percent = tpp
+        self.holidays = h
 
     def add_project(self, p):
         self.projects[p.id] = p
@@ -50,7 +92,7 @@ class Manager:
         agent_by_date = self.agents_by_project_and_date.get(project_id)
         if agent_by_date is None:
             agent_by_date = dict()
-        for curDay in Manager.get_days(assignment.start, assignment.end):
+        for curDay in get_days(assignment.start, assignment.end):
             # save assignment for the day
             agent_list = agent_by_date.get(curDay)
             if agent_list is None:
@@ -79,34 +121,53 @@ class Manager:
         except KeyError:
             return set()
 
-    @staticmethod
-    def get_days(start, end):
-        if start > end:
-            raise Exception("Start date ({}) > end date ({})".format(start, end))
-        cur = start
-        delta = datetime.timedelta(days=1)
-        days = list()
-        while cur <= end:
-            if cur.weekday() <= 4:
-                days.append(cur)
-            cur = cur + delta
-        return days
-
     def summary(self):
         summary = dict()
         for proj_id, proj in self.projects.items():
             proj_report = dict()
-            for day in Manager.get_days(self.start, self.end):
+            for day in get_days(self.start, self.end):
                 day_key = "{}_{}".format(day.year, day.month)
                 val = proj_report.get(day_key)
                 if val is None:
                     val = 0
-                val += len(self.get_agents(proj_id, day))
+
+                for agent in self.get_agents(proj_id, day):
+                    worked_days = opened_days_in_month(day_key) - self.holidays.get(agent, day_key)
+                    worked_days_on_project = worked_days * self.team_proj_percent.get_agent_project_percentage(agent)
+                    worked_days_on_project_per_opened_day = worked_days_on_project / opened_days_in_month(day_key)
+                    to_add = worked_days_on_project_per_opened_day / self.get_assignment_count(agent, day)
+                    val += to_add
+
                 proj_report[day_key] = val
-            summary[proj.name] = proj_report.items()
+
+            d = {}
+            for m, c in proj_report.items():
+                d[m]=round(c, 2)
+            summary[proj.name] = d
         return summary
 
-#fixme : test summary
-#fixme : integrer le % d'activité
-#fixme : integrer les taches multiples
-#fixme : integrer les conges
+
+def get_days(start, end):
+    if start > end:
+        raise Exception("Start date ({}) > end date ({})".format(start, end))
+    cur = start
+    delta = datetime.timedelta(days=1)
+    days = list()
+    while cur <= end:
+        if cur.weekday() <= 4:
+            days.append(cur)
+        cur = cur + delta
+    return days
+
+
+def opened_days_in_month(ym: str):
+    y, m = ym.split("_")
+    y, m = int(y), int(m)
+    tot_days = calendar.monthrange(y, m)[1]
+    tot = 0
+    for i in range(1, tot_days + 1):
+        if datetime.date(y, m, i).weekday() <= 4:
+            tot += 1
+    return tot
+
+# fixme : test summary
--- main_test.py
+++ main_test.py
@@ -3,10 +3,10 @@ import datetime
 import main
 
 
-class TestManagerGetDays(unittest.TestCase):
+class TestGetDays(unittest.TestCase):
 
     def test_nominal(self):
-        days = main.Manager.get_days(datetime.date(2021, 1, 1), datetime.date(2021, 1, 10))
+        days = main.get_days(datetime.date(2021, 1, 1), datetime.date(2021, 1, 10))
         self.assertEqual(len(days), 6)
         self.assertIn(datetime.date(2021, 1, 1), days)
         self.assertIn(datetime.date(2021, 1, 4), days)
@@ -22,7 +22,10 @@ class TestManagerGetDays(unittest.TestCase):
 
 class TestManagerAddProject(unittest.TestCase):
     def test_nominal(self):
-        m = main.Manager(datetime.date(2021, 1, 1), datetime.date(2021, 1, 10), "testdata/projPart.json")
+        m = main.Manager(datetime.date(2021, 1, 1),
+                         datetime.date(2021, 1, 10),
+                         main.TeamProjPercent.empty(),
+                         main.Holidays.empty())
         with self.assertRaises(Exception):
             m.get_project("projId")
         p = main.Project("projId", "projName")
@@ -31,17 +34,17 @@ class TestManagerAddProject(unittest.TestCase):
         self.assertEqual(p, p2)
 
 
-class TestManagerGetAgentProjectPercentage(unittest.TestCase):
+class TestTeamProjPercent(unittest.TestCase):
     def test_nominal(self):
-        m = main.Manager(datetime.date(2021, 1, 1), datetime.date(2021, 1, 10), "testdata/projPart.json")
+        tpp = main.TeamProjPercent.load("testdata/projPart.json")
         with self.assertRaises(Exception):
-            m.get_agent_project_percentage("Robert")
-        self.assertEqual(m.get_agent_project_percentage("John"), 0.7)
-        self.assertEqual(m.get_agent_project_percentage("Doe"), 0.3)
+            tpp.get_agent_project_percentage("Robert")
+        self.assertEqual(tpp.get_agent_project_percentage("John"), 0.7)
+        self.assertEqual(tpp.get_agent_project_percentage("Doe"), 0.3)
 
     def test_file_not_found(self):
         with self.assertRaises(Exception):
-            m = main.Manager(datetime.date(2021, 1, 1), datetime.date(2021, 1, 10), "nonExistingFile.json")
+            tpp = main.TeamProjPercent.load("nonExistingFile.json")
 
 
 class TestManagerAddAssignment(unittest.TestCase):
@@ -51,7 +54,10 @@ class TestManagerAddAssignment(unittest.TestCase):
         #   John    1/1/2021 - 5/1/2021
         # projectB
         #   Bob     2/1/2021 - 6/1/2021
-        m = main.Manager(datetime.date(2021, 1, 1), datetime.date(2021, 1, 10), "testdata/projPart.json")
+        m = main.Manager(datetime.date(2021, 1, 1),
+                         datetime.date(2021, 1, 10),
+                         main.TeamProjPercent.empty(),
+                         main.Holidays.empty())
         m.add_project(main.Project("projectA", "project A name"))
         m.add_assignment("projectA", main.Assignment("Bob", datetime.date(2021, 1, 1), datetime.date(2021, 1, 10)))
         m.add_assignment("projectA", main.Assignment("John", datetime.date(2021, 1, 1), datetime.date(2021, 1, 5)))
@@ -68,7 +74,62 @@ class TestManagerAddAssignment(unittest.TestCase):
         self.assertSetEqual(m.get_agents("projectA", datetime.date(2021, 1, 8)), set(["Bob"]))
         self.assertSetEqual(m.get_agents("projectA", datetime.date(2021, 1, 30)), set())
 
-        print(m.summary())
+
+class TestHolidays(unittest.TestCase):
+    def test_nominal(self):
+        h = main.Holidays.load("testdata/holidays.csv")
+        self.assertEqual(h.get("John", "2021_01"), 5)
+        self.assertEqual(h.get("John", "2021_02"), 3)
+        self.assertEqual(h.get("Doe", "2021_01"), 2)
+        self.assertEqual(h.get("Doe", "2021_02"), 8)
+        self.assertEqual(h.get("Bob", "2021_01"), 0)
+        self.assertEqual(h.get("John", "2021_12"), 0)
+
+    def test_file_not_found(self):
+        with self.assertRaises(Exception):
+            tpp = main.Holidays.load("nonExistingFile.json")
+
+
+class TestHolidays(unittest.TestCase):
+    def test_nominal(self):
+        self.assertEqual(main.opened_days_in_month("2021_1"), 21)
+        self.assertEqual(main.opened_days_in_month("2021_2"), 20)
+
+
+class TestManagerSummary(unittest.TestCase):
+    def test(self):
+        h = main.Holidays.load("testdata/holidays.csv")
+        tpp = main.TeamProjPercent.load("testdata/projPart.json")
+        m = main.Manager(
+            datetime.date(2021, 1, 1),
+            datetime.date(2021, 2, 28),
+            tpp,
+            h
+        )
+
+        # projectA
+        #   Doe     1/1/2021 - 15/2/2021
+        #   John    1/1/2021 - 5/1/2021
+        # projectB
+        #   Doe     1/2/2021 - 28/2/2021
+        # projectC
+        #   John    6/1/2021 - 28/02/2021
+
+        m.add_project(main.Project("projectA", "project A name"))
+        m.add_assignment("projectA", main.Assignment("Doe", datetime.date(2021, 1, 1), datetime.date(2021, 2, 15)))
+        m.add_assignment("projectA", main.Assignment("John", datetime.date(2021, 1, 1), datetime.date(2021, 1, 5)))
+        m.add_project(main.Project("projectB", "project B name"))
+        m.add_assignment("projectB", main.Assignment("Doe", datetime.date(2021, 2, 1), datetime.date(2021, 2, 28)))
+        m.add_project(main.Project("projectC", "project C name"))
+        m.add_assignment("projectC", main.Assignment("John", datetime.date(2021, 1, 6), datetime.date(2021, 2, 28)))
+
+        res = m.summary()
+        self.assertEqual(res["project A name"]["2021_1"], 7.3)
+        self.assertEqual(res["project A name"]["2021_2"], 0.99)
+        self.assertEqual(res["project B name"]["2021_1"], 0)
+        self.assertEqual(res["project B name"]["2021_2"], 2.61)
+        self.assertEqual(res["project C name"]["2021_1"], 9.6)
+        self.assertEqual(res["project C name"]["2021_2"], 11.9)
 
 
 if __name__ == '__main__':
